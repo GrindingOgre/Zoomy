ec2139998f3ea5284f6d2d07d35ccff60426e8bc
Marco <agostino.marco@gmail.com>
2018-06-21 11:20:03 +0200

    Create DoubleTapListener.java

---
+++ zoomy/src/main/java/com/ablanco/zoomy/DoubleTapListener.java
@@ -0,0 +1,13 @@
+package com.ablanco.zoomy;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Marco Agostino on 20/06/2018.
+ * Zoomy.
+ */
+
+public interface DoubleTapListener {
+    void onDoubleTap(View v, MotionEvent event);
+}
